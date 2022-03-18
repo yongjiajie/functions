2a93abc495195df6f7d266796ecc221613294667
Yong Jia Jie <yongjiajie.govtech@gmail.com>
2022-03-18 18:20:31 +0800

    feat: initial commit

---
+++ netlify/functions/crc.ts
@@ -0,0 +1,7 @@
+import { Handler } from "@netlify/functions";
+
+const handler: Handler = async (event, context) => {
+  return { statusCode: 200, body: JSON.stringify({ message: "Hello world!" }) };
+};
+
+export { handler };
